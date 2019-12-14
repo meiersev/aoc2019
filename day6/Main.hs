5b6294af61cd96358dec4ce9f0bf60de11c9dc77
Severin Meier <serv.meier@gmail.com>
2019-12-15 00:04:30 +0100

    added day6 solution

---
+++ day6/Main.hs
@@ -0,0 +1,52 @@
+import Data.Map.Strict hiding (map)
+
+inputFile = "input"
+
+data Object = COM | Object { name :: String, center :: String } deriving Show
+
+separator = (/=) ')'
+
+parseSingle :: String -> Object
+parseSingle str = let 
+    center = takeWhile separator str
+    name = tail (dropWhile separator str)
+    in Object name center
+
+parseObjects :: [String] -> Map String Object
+parseObjects ls = let fromIn = fromList (map (\str -> let obj = parseSingle str in (name obj, obj)) ls) in
+    insert "COM" COM fromIn
+
+nOrbitsClosure :: Object -> Map String Object -> Int
+nOrbitsClosure COM _ = 0
+nOrbitsClosure obj m = 1 + nOrbitsClosure (m ! (center obj)) m
+
+pathToRoot :: Map String Object -> String -> [String]
+pathToRoot _ "COM" = []
+pathToRoot m key = let cent = center (m ! key) in cent : pathToRoot m cent
+
+lca :: Map String Object -> String -> String -> String
+lca m a b = let 
+    pTa = pathToRoot m a
+    pTb = pathToRoot m b
+    zippedPaths = zip (reverse pTa) (reverse pTb)
+    firstDiff = head (dropWhile (\(x, y) -> x == y) zippedPaths)
+    in
+        center (m ! (fst firstDiff))
+
+jumps :: Map String Object -> String -> String -> Int
+jumps m a b = let
+    pTa = pathToRoot m a
+    pTb = pathToRoot m b
+    lcaAB = lca m a b
+    distToLca = \path -> length (takeWhile ((/=) lcaAB) path)
+    in
+        distToLca pTa + distToLca pTb
+
+main = do
+    input <- readFile inputFile
+    let m = parseObjects (lines input)
+    -- print (toList m)
+    let resultPart1 = sum (map (\el -> nOrbitsClosure el m) (elems m))
+    print resultPart1
+    print (jumps m "YOU" "SAN")
+
