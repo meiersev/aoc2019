d6d563334028c4bc6fc528d3fa0309431b785e41
Severin Meier <serv.meier@gmail.com>
2019-12-23 13:20:05 +0100

    added option to run Intcode with IO

--- aoc/src/Intcode.hs
+++ aoc/src/Intcode.hs
@@ -1,4 +1,4 @@
-module Intcode(run) where 
+module Intcode(run, runWithIO) where 
 
 import Utils
 
@@ -14,12 +14,43 @@ doAdd pos state = doOp pos state (+)
 doMul :: Int -> [Int] -> Int
 doMul pos state = doOp pos state (*)
 
-runStep :: Int -> [Int] -> (Int -> [Int] -> Int) -> [Int]
-runStep pos state op = replace (state!!(pos+3)) (op pos state) state
+doBiFunc :: Int -> [Int] -> (Int -> [Int] -> Int) -> [Int]
+doBiFunc pos state op = replace (state!!(pos+3)) (op pos state) state
+
+inputInstr :: Int -> [Int] -> IO [Int]
+inputInstr pos state = do
+    input <- getLine
+    let i = read input :: Int
+    return (replace (state!!(pos+1)) i state)
+
+outputInstr :: Int -> [Int] -> IO [Int]
+outputInstr pos state = do
+    print (valAt (pos+1) state)
+    return state
+
+runOp :: Int -> Int -> [Int] -> (Int, [Int])
+runOp _ 99 state = (-1, state) 
+runOp pos 1 state = (pos+4, doBiFunc pos state doAdd) 
+runOp pos 2 state = (pos+4, doBiFunc pos state doMul) 
+runOp _ x _ = error ("unsupported operation " ++ (show x))
+
+runOpWithIO :: Int -> Int -> [Int] -> (Int, IO [Int])
+runOpWithIO pos 99 state = (pos, return state)
+runOpWithIO pos 1 state = let (newPos, newState) = runOp pos 1 state in (newPos, return newState)
+runOpWithIO pos 2 state = let (newPos, newState) = runOp pos 2 state in (newPos, return newState)
+runOpWithIO pos 3 state = (pos+2, inputInstr pos state)
+runOpWithIO pos 4 state = (pos+2, outputInstr pos state)
+runOpWithIO _ x _ = error ("unsupported operation " ++ (show x))
+
+runWithIO :: Int -> IO [Int] -> IO [Int]
+runWithIO pos state = do
+    s <- state
+    let op = s!!pos
+        (nextPos, nextState) = runOpWithIO pos op s
+    if (nextPos < 0) then nextState
+    else runWithIO nextPos nextState
 
 run :: Int -> [Int] -> [Int]
-run pos state
-    | state!!pos == 99 = state
-    | state!!pos == 1 = run (pos + 4) (runStep pos state doAdd)
-    | state!!pos == 2 = run (pos + 4) (runStep pos state doMul)
-    | otherwise = [-1]
+run pos state = let (nextPos, nextState) = runOp pos (state!!pos) state in
+    if (nextPos < 0) then nextState
+    else run nextPos nextState
