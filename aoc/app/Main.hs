6fb67c388fb09118bbbda3bfde4852572a27b9ff
Severin Meier <serv.meier@gmail.com>
2019-12-23 15:07:39 +0100

    added day5 part 1 solution

--- aoc/app/Main.hs
+++ aoc/app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Day5
 
 main :: IO ()
-main = testIOOps
+main = runProgPart1
--- aoc/src/Day5.hs
+++ aoc/src/Day5.hs
@@ -1,6 +1,9 @@
 module Day5 where
 
 import Intcode
+import Utils
+
+inputFile = "test/resources/day5/input"
 
 testInputReadWrite = [3,0,4,0,99]
 testInputParameterModes = [1002,4,3,4,33]
@@ -9,3 +12,11 @@ testIOOps :: IO()
 testIOOps = do
     endState <- runWithIO 0 (return testInputReadWrite)
     print endState
+
+runProgPart1 :: IO()
+runProgPart1 = do
+    input <- readFile inputFile
+    let split = splitString input ','
+        numbers = return (map (\s -> read s :: Int) split)
+    runWithIO 0 numbers
+    return ()
