da105a809746783ae5ea34c58f090587c1e0198b
Severin Meier <serv.meier@gmail.com>
2019-12-24 15:17:04 +0100

    added part 2 solution

--- aoc/src/Day7.hs
+++ aoc/src/Day7.hs
@@ -7,8 +7,10 @@ import Intcode
 phases :: [[Int]]
 phases = permutations [0..4]
 
-testInput :: [Int]
+testInput :: Program
 testInput = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0] 
+testInput2 :: Program
+testInput2 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
 
 inputFile = "test/resources/day7/input"
 
@@ -30,16 +32,65 @@ tryAllPhases (p:ps) prog = let
     other = tryAllPhases ps prog
     in if nextVal > fst other then (nextVal, p) else other
 
+input :: IO [Int]
+input = do
+    i <- readFile inputFile
+    let split = splitString i ','
+    let prog = map (\s -> read s :: Int) split
+    return prog
+
 runPart1 :: IO ()
 runPart1 = do
-    input <- readFile inputFile
-    let split = splitString input ','
-        numbers = map (\s -> read s :: Int) split
-    print $ tryAllPhases phases numbers
+    prog <- input
+    print $ tryAllPhases phases prog
 
 -- Part 2
 phasesPt2 :: [[Int]]
 phasesPt2 = permutations [5..9]
 
-initAmplifiers :: Program -> [Program]
-initAmplifiers prog = replicate 5 prog
+initAmplifiers :: Program -> [Int] -> [ProgramContext]
+initAmplifiers prog phases = ampA : amps
+  where
+    ampA = ProgramContext prog 0 [head phases, 0] [] Ready 
+    amps = map (\p -> ProgramContext prog 0 [p] [] Ready) $ tail phases
+
+anyAmpWithStatus s = any (\amp -> status amp == s)
+
+moveOutputToInputs :: [ProgramContext] -> [ProgramContext]
+moveOutputToInputs ctxs = map clearOutputs $ map updateInputs zipped
+  where
+    zipped = zip (rotateList 1 ctxs) ctxs
+    updateInputs (from,to) = 
+        if (length $ outputs from) > 0 
+        then to { inputs = [last $ outputs from], status=Ready } 
+        else to
+    clearOutputs ctx = ctx { outputs=[] }
+
+runAmpsToCompletion :: [ProgramContext] -> [ProgramContext]
+runAmpsToCompletion amps = 
+    if anyAmpWithStatus Ready amps
+    then runAmpsToCompletion $ map run amps
+    else
+        if anyAmpWithStatus Suspended amps
+        then runAmpsToCompletion $ moveOutputToInputs amps
+        else amps -- all finished
+
+thrusterSignal :: [ProgramContext] -> Int
+thrusterSignal ctxs = head $ outputs $ last ctxs
+
+maxSignalFromPhase :: [[Int]] -> Program -> (Int, [Int])
+maxSignalFromPhase (p:[]) prog = (thrusterSignal $ runAmpsToCompletion $ initAmplifiers prog p, p) 
+maxSignalFromPhase (p:ps) prog = 
+    if thisSignal > fst other
+    then (thisSignal , p)
+    else other
+  where
+    ctxs = initAmplifiers prog p
+    thisSignal = thrusterSignal $ runAmpsToCompletion ctxs 
+    other = maxSignalFromPhase ps prog
+
+runPart2 :: IO ()
+runPart2 = do
+    prog <- input
+    print $ maxSignalFromPhase phasesPt2 prog
+    return ()
