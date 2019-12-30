00af37c1d7bc89ff3155e8552d73d68acae865e4
Severin Meier <serv.meier@gmail.com>
2019-12-30 16:45:21 +0100

    added tests for large numbers

--- aoc/test/IntcodeSpec.hs
+++ aoc/test/IntcodeSpec.hs
@@ -69,3 +69,9 @@ spec =
         it "can run quine" $ 
             let quine = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] in
             (outputs $ runSimple quine) `shouldBe` reverse quine
+
+        it "can handle large numbers" $
+            (outputs $ runSimple [1102,34915192,34915192,7,4,7,99,0]) `shouldBe` [1219070632396864]
+
+        it "can handle large numbers 2" $
+            (outputs $ runSimple [104,1125899906842624,99]) `shouldBe` [1125899906842624]
